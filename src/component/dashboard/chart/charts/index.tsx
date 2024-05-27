05a5b764c427ab52d0e6131c272bacc4b5279ced
cledson leite <csbetsonline@gmail.com>
2024-05-27 02:10:14 -0300

    feat<frontend>: implement loading component

--- src/component/dashboard/chart/charts/index.tsx
+++ src/component/dashboard/chart/charts/index.tsx
@@ -1,12 +1,18 @@
+import { Suspense } from "react"
 import { KWHChart } from "../kwh-chart"
 import { ValueChart } from "../value-chart"
 import style from './chart.module.scss'
+import { Loading } from "../../loading"
 
 export const Charts = () => {
     return (
         <section className={style.container}>
-            <KWHChart />
-            <ValueChart />
+            <Suspense fallback={<Loading />}>
+                <KWHChart />
+            </Suspense>
+            <Suspense fallback={<Loading />}>
+                <ValueChart />
+            </Suspense>
         </section>
     )
 }
---
+++ src/component/dashboard/loading/index.tsx
@@ -0,0 +1,7 @@
+import CircularProgress from '@mui/material/CircularProgress';
+
+export const Loading = () => {
+    return (
+        <CircularProgress />
+    )
+}
